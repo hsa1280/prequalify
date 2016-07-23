e6cc78bbf97eef882912df3c4ec7508393c3a9a2
Shian Huang <shian.huang@manheim.com>
2016-07-23 10:27:24 -0400

    Added user input form and form validation

--- src/main-view/prequalifyController.js
+++ src/main-view/prequalifyController.js
@@ -1,7 +1,48 @@
+const annotation = ['$http'];
+
 class prequalifyController {
-  constructor() {
-    this.name = 'shian huang';
+  constructor($http) {
+    this.$http = $http;
+    this.formSubmitted = false;
+    this.typeOfBusinessArr = ['Accounting', 'Amusement', 'AutoRepair', 'BusinessServices', 'Catering', 'ChildCare', 'ComputerServices', 'ConsumerGoodsRetailStore', 'ConsumerGoodsOnlineStore', 'ConsumerGoodsOnlineAndOffline', 'Construction', 'Dentists', 'DryCleaning', 'Equipment', 'Grocery', 'Health', 'HomeRepair', 'Hotels', 'Insurance', 'Janitorial', 'Landscape', 'Optometrists', 'Physicians', 'Restaurants', 'Salons', 'Taxis', 'Trucking', 'Veterinarians'];
   }
+
+  successCallback(response) {
+    console.log('success', response);
+  }
+
+  errorCallback(response) {
+    console.log('error', response);
+  }
+
+  submitForm() {
+    this.formSubmitted = true;
+    if (this.myForm.$valid) {
+      this.$http({
+        method: 'POST',
+        url: 'http://api.kabbage.com/v2/prequalify',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: {
+          firstName: this.userInfo.firstName,
+          lastName: this.userInfo.lastName,
+          emailAddress: this.userInfo.email,
+          businessName: this.userInfo.businessName,
+          phoneNumber: this.userInfo.phoneNumber,
+          yearStarted: this.userInfo.yearStarted,
+          estimatedFICO: this.userInfo.estimatedFICO,
+          estimatedAnnualRevenue: this.userInfo.estimatedAnnualRevenue,
+          grossPercentageFromCards: this.userInfo.grossPercentageFromCards,
+          typeOfBusiness: 'Accounting',
+          api_key: this.userInfo.api_key
+        }
+      }).
+      then(this.successCallback, this.errorCallback);
+    }
+  }
+
 }
 
+prequalifyController.$inject = annotation;
 export default prequalifyController;
